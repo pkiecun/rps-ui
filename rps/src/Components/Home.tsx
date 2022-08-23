4c2240e5d7ccbbe986ec0ca5fc01e6e76fed16be
Link <2211114@cognizant.com>
2022-08-23 12:36:16 -0400

    vs cpu functionality done'

--- rps/src/Components/Home.tsx
+++ rps/src/Components/Home.tsx
@@ -1,24 +1,82 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { Result } from "./Result";
 import RockP1 from "../Images/RockP1.jpg";
 import PaperP1 from "../Images/PaperP1.jpg";
 import ScissorsP1 from "../Images/ScissorsP1.jpg";
-import RockP2 from "../Images/RockP2.jpg";
-import PaperP2 from "../Images/PaperP2.jpg";
-import ScissorsP2 from "../Images/ScissorsP2.jpg";
 import { IRound } from "../Interfaces/IRound";
+import { IGame } from "../Interfaces/IGame";
+import { AppDispatch, RootState } from "../Store";
+import { useDispatch, useSelector } from "react-redux";
+import { soloGame } from "../Slices/GameSlice";
 
 export const Home: React.FC = () => {
 
-  const [round, setRound] = useState<IRound>({userChoice: "0", opponentChoice: "0"});
-  
-  const handleUserChoice = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const [round, setRound] = useState<IRound>({userChoice: 0, opponentChoice: 0, winner: 0});
+  const [record, setRecord] = useState<IGame>({matchTo: 3, wins: 0, losses: 0});
+  const oppMove = useSelector((state:RootState) => state.game);
+  const dispatch: AppDispatch = useDispatch();
 
-      setRound({
-        userChoice:event.currentTarget.value,
-        opponentChoice:"1"
-      })
+
+  const chooseWinner = (round: IRound) => {
+    
+      
+      if((round.userChoice === 1  && round.opponentChoice === 3)
+          || (round.userChoice == 2 && round.opponentChoice === 1)
+          || (round.userChoice === 3 && round.opponentChoice === 2)){
+        round.winner = 1;
+        setRound({
+          userChoice:round.userChoice,
+          opponentChoice:round.opponentChoice,
+          winner: 1
+        })
+      }
+      else if((round.userChoice === 1 && round.opponentChoice === 2)
+              || (round.userChoice === 2 && round.opponentChoice === 3)
+              || (round.userChoice === 3 && round.opponentChoice === 1)) {
+        round.winner = 2;
+        setRound({
+          userChoice:round.userChoice,
+          opponentChoice:round.opponentChoice,
+          winner: 2
+        })
+      } else {
+        round.winner = 0;
+        setRound({
+          userChoice:round.userChoice,
+          opponentChoice:round.opponentChoice,
+          winner: 0
+        })
+      }
+  }
+
+  const handleUserChoice = async (event: React.MouseEvent<HTMLButtonElement>) => {
+      
+      round.userChoice = parseInt(event.currentTarget.value);
+
+      dispatch(soloGame());
+      round.opponentChoice = oppMove.opponentChoice;
+
+      chooseWinner(round);
+
+      if(round.winner === 1){
+        record.wins += 1;
+      } else if(round.winner === 2){
+        record.losses += 1;
+      }
+
+  }
+
+  const handlePlayAgain = () => {
+    setRound({
+      userChoice: 0,
+      opponentChoice: 0,
+      winner: 1
+    })
+  }
+
+  const makeJustinHappy = () => {
+    
   }
 
   return (
@@ -26,15 +84,14 @@ export const Home: React.FC = () => {
       <div className="result-page">
         <div className="wins-losses">
             <div className="wins">Wins
-                <div className="win-count">0</div>
+                <div className="win-count">{record.wins}</div>
             </div>
             <div className="losses">Losses
-                <div className="loss-count">0</div>
+                <div className="loss-count">{record.losses}</div>
             </div>
         </div>
-        <div className="game-result">You win</div>
       </div>
-      {round.userChoice == "0" ? 
+      {round.userChoice === 0 ? 
         <>
         <h1>CHOOSE!</h1>
         <table className="table">
@@ -48,7 +105,8 @@ export const Home: React.FC = () => {
         </table> 
         </>: <>
           <Result {...round}/>
-          <button className="play-again" value="0" onClick={handleUserChoice}>Play Again</button>
+          <button className="play-again" value="0" onClick={handlePlayAgain}>Play Again</button>
+          <button onClick={makeJustinHappy}>Justin is very happy</button>
           </>} 
     </div>
   );
